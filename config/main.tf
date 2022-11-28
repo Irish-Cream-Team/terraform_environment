8a0f4d4a96edb85cfbd8f9c23f773daa616c430d
fishers-51-vm <fishers-51-vm@azure-user>
2022-11-28 16:50:09 +0000

    update: latest varsion

--- config/main.tf
+++ config/main.tf
@@ -1,12 +1,14 @@
 module "setup" {
-  source                     = "github.com/Irish-Cream-Team/Terraform_moudles.git//Setup?ref=2f73c73a2c366552f04f0f973b9c623e7966189e"
+  source                     = "github.com/Irish-Cream-Team/Terraform_moudles.git//Setup?ref=da1a449ad1452e688c90c8b7121be61dfc08a34c"
   location                   = var.location
   global_resource_group_name = var.global_resource_group_name
   team_name                  = var.team_name
+  tags                       = var.tags
+
 }
 
 module "vm" {
-  source = "github.com/Irish-Cream-Team/Terraform_moudles.git//VM?ref=2f73c73a2c366552f04f0f973b9c623e7966189e"
+  source = "github.com/Irish-Cream-Team/Terraform_moudles.git//VM?ref=da1a449ad1452e688c90c8b7121be61dfc08a34c"
 
   team_name = var.team_name
   count     = length(var.VMNames)
