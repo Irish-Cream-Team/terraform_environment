c16aaef0ef39af40eedc8dd58f2c162309520d82
fishers-51-vm <fishers-51-vm@azure-user>
2022-11-28 12:08:52 +0000

    add: vm dns name to output

--- config/outputs.tf
+++ config/outputs.tf
@@ -4,9 +4,8 @@ output "vms_info" {
     {
       vm_name     = vm.vm_name,
       vm_username = vm.vm_username,
-      vm_ip       = vm.vm_ip
+      vm_ip       = vm.vm_ip,
+      vm_dns      = vm.vm_dns.fqdn
     }
   ]
 }
-
-
