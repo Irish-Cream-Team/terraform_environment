49cb86481605059eaceba2ed92a66ce71ad6c0de
fishers-51-vm <fishers-51-vm@azure-user>
2022-12-20 11:08:06 +0000

    change: remove the need of global_resource_group_name var

--- Teams_vms/Terraform_config/variables.tf
+++ Teams_vms/Terraform_config/variables.tf
@@ -5,7 +5,8 @@ variable "location" {
 }
 
 variable "team_name" {
-  type = string
+  type        = string
+  description = "The name of the team"
 }
 
 variable "VM" {
@@ -13,6 +14,7 @@ variable "VM" {
     name       = string
     public_key = optional(any)
   }))
+  description = "This is a list of VMs objects, each object must contains the name of the VM and its optional to send the public key to be used to access the VM"
 }
 
 variable "tags" {
@@ -24,5 +26,7 @@ variable "tags" {
 
 
 variable "global_resource_group_name" {
-  type = string
+  type    = string
+  default = "global"
+
 }
